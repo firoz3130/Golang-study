dbc3009bc0e6b9d35c74caca03d3b5cc96f6b0af
firoz3130 <firosk7@gmail.com>
2023-07-25 22:40:54 +0530

    Loops committed

---
+++ lib/loop.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sum int
+	sum = 0
+	fmt.Println("Enter the number of elements in array")
+	var n int
+	fmt.Scanln(&n)
+	//enter the elements of the array from user input
+	num := make([]int, n)
+	fmt.Printf("Enter %d integers to the array ", n)
+	for i := 0; i < n; i++ {
+		fmt.Scanln(&num[i])
+	}
+	fmt.Println("The elements of the array are: ")
+	for i := 0; i < n; i++ {
+		fmt.Println(num[i])
+		sum += num[i]
+	}
+	fmt.Println("Sum: ", sum)
+	//while loop
+	// count := 10
+	// for count < 0 {
+	// 	fmt.Println("Counter---- ", count)
+	// 	count--
+	// }
+	// fmt.Println("Stopped the counter")
+	count := 10
+	for count > 0 {
+		fmt.Println(count)
+		count--
+	}
+	fmt.Println("Liftoff!")
+}
